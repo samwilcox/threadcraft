452c7c9f50e1dd09798306f542f96c6d5980673a
Sam Wilcox <wilcox.sam@gmail.com>
2023-12-01 04:41:03 -0700

    Closes #2 (#5)

---
+++ server/src/test/java/com/threadcraft/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadcraft.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
