ba944ec2afbeb120436577c0fb330d0f320cfb5f
Sam <samwilcox@u.boisestate.edu>
2023-12-14 01:43:54 -0700

    Removing Spring Boot and replacing with PHP

---
+++ threadcraftui/src/Test.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+function Test() {
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                let response = await fetch('http://localhost:8080/sam', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.ok) {
+                    const responseData = response.json();
+                    console.log("Success!");
+                    console.log(responseData);
+                } else {
+                    console.log("Failed...");
+                }
+            } catch (error) {
+                console.log("Errored out:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>Test</div>
+    );
+}
+
+export {Test}
